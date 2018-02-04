12b3be61987bc6cb9a4c9cb3b1c292e7c880db1d
GageMerrick <gtmerrick@plymouth.edu>
2018-02-04 14:33:10 -0500

    Added new Item
    Also save and load were updated to support the newer items

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ var BCRate = 0;
 var AverageComputers = 0;
 var ACRate = 0;
 
+var DecentComputers = 0;
+var DCRate = 0;
+
 function Save(){
   var save = {
       GCoins: GCoins,
-      BadComputers: BadComputers
+      BadComputers: BadComputers,
+      AverageComputers : AverageComputers,
+      DecentComputers : DecentComputers
   }
   localStorage.setItem("save",JSON.stringify(save));
 }
@@ -18,6 +23,8 @@ function Load(){
   var savegame = JSON.parse(localStorage.getItem("save"));
   if (typeof savegame.GCoins !== "undefined") GCoins = savegame.GCoins;
   if (typeof savegame.BadComputers !== "undefined") BadComputers = savegame.BadComputers;
+  if (typeof savegame.AverageComputers !== "undefined") AverageComputers = savegame.AverageComputers;
+  if (typeof savegame.DecentComputers !== "undefined") DecentComputers = savegame.DecentComputers;
 }
 
 function Mine(ammount){
@@ -37,6 +44,12 @@ function AverageComputer(n){
   ACRate = ((n*0.0000000000001)*10);
 }
 
+function DecentComputer(n){
+  //Calculate how much your DecentComputer helps
+  Mine(n*0.000000000001);
+  DCRate = ((n*0.000000000001)*10);
+}
+
 function Purchase(selection){
   //Purchase an item
   if(selection == 'BC'){
@@ -45,6 +58,9 @@ function Purchase(selection){
   }else if(selection == 'AC'){
     Mine(-0.000000000150);
     AverageComputers += 1;
+  }else if(selection == 'DC'){
+    Mine(-0.000000000500);
+    DecentComputers += 1;
   }
 }
 
@@ -52,6 +68,7 @@ function UpdateText(){
   //update the text on the page
   document.getElementById("BComputers").innerHTML = BadComputers + "  (" + BCRate.toFixed(12)+"/s" + ")";
   document.getElementById("AComputers").innerHTML = AverageComputers + "  (" + ACRate.toFixed(12)+"/s" + ")";
+  document.getElementById("ADomputers").innerHTML = DecentComputers + "  (" + DCRate.toFixed(12)+"/s" + ")";
   document.getElementById("Coins").innerHTML = GCoins.toFixed(12);
 }
 
@@ -67,11 +84,18 @@ function UpdatePurchasableItems(){
   }else{
     document.getElementById('BuyAComp').disabled = true;
   }
+
+  if(GCoins >= 0.000000000500){
+    document.getElementById('BuyDComp').disabled = false;
+  }else{
+    document.getElementById('BuyDComp').disabled = true;
+  }
 }
 
 function autoMiners(){
   BadComputer(BadComputers);
   AverageComputer(AverageComputers);
+  DecentComputer(DecentComputers);
 }
 
 window.setInterval(function(){
